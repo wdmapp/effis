32f5b740e04b126b40f1476d3dfd1fd3c86b712f
Eric Suchyta <eric.d.suchyta@gmail.com>
2025-12-03 08:13:40 -0500

    Generic Manager() command with Campaign

--- src/effis/composition/hpc_campaign.py
+++ src/effis/composition/hpc_campaign.py
@@ -59,16 +59,23 @@ class Campaign:
                 cmd += ["--{0}".format(name), attr]
         return cmd + [self.filename]
 
+    def Manager(
+        self,
+        cmd,
+        *args
+    ):
+        cmd = self._manager_ + [cmd, *args]
+        subprocess.call(cmd)
+
     def Create(self):
         if not os.path.exists(self.filename):
-            cmd = self._manager_ + ["create"]
-            subprocess.call(cmd)
+            self.Manager("create")
         else:
             CompositionLogger.Warning(
                 "{0} already exists. Skipping create.".format(self.filename)
             )
 
-    def AddFile(self, filename, name=None):
+    def Dataset(self, filename, name=None):
         self.CheckString(name, "name")
         if not os.path.isfile(self.filename):
             CompositionLogger.Warning(
@@ -76,8 +83,13 @@ class Campaign:
                     self.filename
                 )
             )
+        elif not os.path.exists(filename):
+            CompositionLogger.Warning(
+                "{0} does not exist. Skipping dataset add.".format(
+                    filename
+                )
+            )
+        elif name is not None:
+            self.Manager("dataset", filename, "--name", name)
         else:
-            cmd = self._manager_ + ["dataset", filename]
-            if name is not None:
-                cmd += ["--name", name]
-            subprocess.call(cmd)
+            self.Manager("dataset", filename)
